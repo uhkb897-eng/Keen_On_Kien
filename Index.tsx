647dc417511e7a7dc0397c044537544141764491
eddie <uhkb897@gmail.com>
2025-11-18 23:50:27 +0700

    Update Index.tsx

--- Index.tsx
+++ Index.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import { PuzzleGame } from "@/components/PuzzleGame";
-import { QuizSection } from "@/components/QuizSection";
-import { VideoSection } from "@/components/VideoSection";
-import { CongratulationsSection } from "@/components/CongratulationsSection";
+import { PuzzleGame } from "@/PuzzleGame";
+import { QuizSection } from "@/QuizSection";
+import { VideoSection } from "@/VideoSection";
+import { CongratulationsSection } from "@/CongratulationsSection";
 
 type Stage = "puzzle" | "quiz" | "video" | "congratulations";
 
